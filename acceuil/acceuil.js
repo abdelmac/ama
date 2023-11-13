b1db75bfd576543028eba31d850435fc608472e5
abdelmac <abdelkadermachta@gmail.com>
2023-11-13 01:38:05 +0100

    de base

--- acceuil/acceuil.js
+++ acceuil/acceuil.js
@@ -4,12 +4,17 @@ const navbar = document.getElementById('navbar');
 // Récupérez la position initiale de la barre de navigation
 const navbarOffset = navbar.offsetTop;
 
+// Récupérez la hauteur de la barre de navigation
+const navbarHeight = navbar.clientHeight;
+
 // Fonction pour mettre à jour la position de la barre de navigation lors du défilement
 function updateNavbar() {
     if (window.pageYOffset >= navbarOffset) {
         navbar.classList.add('fixed');
+        document.body.style.marginTop = navbarHeight + 'px'; // Ajoutez une marge équivalente à la hauteur de la barre
     } else {
         navbar.classList.remove('fixed');
+        document.body.style.marginTop = '0'; // Réinitialise la marge
     }
 }
 
